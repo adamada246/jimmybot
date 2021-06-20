59a47eef6fcbcea6b6f5ebf86c54369f4c90964f
Adam Schneid <78761055+adamada246@users.noreply.github.com>
2021-06-20 01:39:37 -0400

    finished gamble command

--- commands/gamble.js
+++ commands/gamble.js
@@ -1,33 +1,42 @@
 const Discord = require('discord.js');
 const { MessageButton, MessageActionRow } = require('discord-buttons')
-
+client.gamblecooldown = new Map();
+client.usergamble = new Map();
 path = require('path'),
 __parentDir = path.dirname(module.parent.filename);
 money = require(__parentDir+'/storage/money.json'); // path may vary
 prefix = require(__parentDir+'/storage/prefix.json'); // path may vary
-client.gamblemessage = new Map();
 
   module.exports = {
 	name: 'gamble',
 	description: 'ok',
 	guildOnly: false,
 	execute(message ) {
-        client.gamblemessage.set(message.author.id, message);
+     
         
+
+    if(!client.gamblecooldown.get(message.author.id)){
+        client.gamblecooldown.set(message.author.id, "false")
+      }
+  
+      if (client.gamblecooldown.get(message.author.id) == "false"){
         const args = message.content.slice(prefix[message.guild.id].length).trim().split(/ +/g);
 
         if (args[1]){
      
            
             if (Number(args[1]) > 0){
+
+               
+                if( Number(args[1]) < money[message.author.id]){
                 //main code goes in here
-                let gambleamt = Number(args[1])
+                let gambleamt = Number(args[1]).toFixed(0)
 
 		const confirmation = new Discord.MessageEmbed()
 		.setColor('#f0dc66')
 		.setTitle('Gamble `'+gambleamt+"` treats?")
 		.setDescription("Disclaimer: **LOSING UP TO 100% OF THE MONEY GAMBLED IS A POSSIBILITY, BE WARNED**")
-		.setFooter('You currently have `'+money[message.author.id]+"` treats total.");
+		.setFooter('You currently have '+money[message.author.id]+" treats total.");
 
         
 
@@ -56,21 +65,36 @@ client.gamblemessage = new Map();
 
         })
 
-        
+        client.usergamble.set(message.author.id, gambleamt);
 
 
 
 
 //end main section of code
+    }
+    else if (Number(args[1]) > money[message.author.id]){
+        message.channel.send("You do not have enough money to gamble this much!",{
+            ephemeral: true
+        })
+    }
             }
             else{
-                message.channel.send("That's not a valid integer! The money gambled must be a whole number above 0.")
+                message.channel.send("That's not a valid integer! The money gambled must be a whole number above 0.",{
+                    ephemeral: true
+                })
             }
         }
         else{
-            message.channel.send("That's not a valid integer! The correct format for this command is `"+prefix[message.guild.id]+"gamble [amount]`.")
+            message.channel.send("That's not a valid integer! The correct format for this command is `"+prefix[message.guild.id]+"gamble [amount]`.",{
+                ephemeral: true
+            })
         }
-    
+      }
+      else{
+        message.channel.send("Slow down there, "+message.author.tag+"! You're on cooldown! You can only run this command every 2 minutes.",{
+            ephemeral: true
+        })
+      }
 
         
 	},
--- events/clickButton.js
+++ events/clickButton.js
@@ -4,7 +4,10 @@ const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('
 const ms = require('ms');
 const { MessageButton, MessageActionRow } = require('discord-buttons')
 
-
+path = require('path'),
+__parentDir = path.dirname(module.parent.filename);
+money = require(__parentDir+'/storage/money.json'); // path may vary
+prefix = require(__parentDir+'/storage/prefix.json'); // path may vary
 
 
 module.exports = async (client, button) => {
@@ -14,7 +17,7 @@ module.exports = async (client, button) => {
     user = await client.users.fetch(userID)
 
     await button.clicker.fetch();
-
+try{
     if(button.id.startsWith("DEVTEST_1")){
         await button.clicker.fetch();
         if (button.clicker.member.hasPermission('KICK_MEMBERS')){
@@ -54,10 +57,60 @@ module.exports = async (client, button) => {
 
 
       if(button.id.startsWith("GAMBLE_1")){
+        
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
         await button.clicker.fetch();
     
+        
         if (button.clicker.user.id == userID){
-            console.log("gambling")
+          if (client.gamblecooldown.get(userID) == "false"){
+            if (Number(client.usergamble.get(userID)) < Number(money[userID])){
+            percentprofit = (getRandomInt(200) - 100)/100
+      
+            
+            newmoneygamble = Number(Number((client.usergamble.get(userID) * percentprofit)).toFixed(0)) + Number(money[userID])
+            moneynet = Number(client.usergamble.get(userID) * percentprofit).toFixed(0)
+            money[userID] = newmoneygamble
+            fs.writeFileSync(__parentDir+'/storage/money.json', JSON.stringify(money))
+           
+            
+    
+            client.gamblecooldown.set(userID, "true")
+           
+
+            const gambleoutcome = new Discord.MessageEmbed()
+		.setColor('#f0dc66')
+		.setTitle('Gamble outcome!')
+    .addFields(
+      { name: moneynet > 0 ? "You gained money!" : "You lost money :(", value: moneynet > 0 ? "You gained `"+moneynet+"` treats!" : "You lost `"+Number(moneynet)*-1+"` treats...", inline: false }
+    )
+		.setFooter('You currently have '+money[userID]+" treats total.");
+
+
+         
+            button.reply.send(button.clicker.user.toString() + ", ", {
+
+              embed: gambleoutcome
+            })
+            setTimeout(() => {
+              client.gamblecooldown.set(userID, "false")
+            }, ms("2m")); 
+          }
+          else{
+            button.reply.send(button.clicker.user.toString()+", you don't have enough money to gamble this much!")
+          }
+
+          
+          
+        }
+        else{
+          button.reply.send("Slow down there, "+button.clicker.user.toString()+"! You're on cooldown! This command can only be executed every 2 minutes.",{
+            ephemeral: true
+          })
+        }
         }
         else{
         button.reply.send(button.clicker.user.toString() + ", you didn't execute this command, you can't press the buttons!",{
@@ -82,7 +135,22 @@ module.exports = async (client, button) => {
         })
         }
       }
-      
+    }catch (error) {
+      await button.guild
+      const channel = button.guild.channels.cache.find(channel => channel.type === 'text' && channel.permissionsFor(button.guild.me).has('SEND_MESSAGES'))
+      if (channel){
+        const errorem = new Discord.MessageEmbed()
+		.setColor('#CC0909')
+		.setTitle('Jimmybot ran into an error. If this persists join our support server, which you can access by pinging the bot.')
+		.setDescription("Button which caused the error: `"+button.id+"`. The error: `"+error+"`.")
+		.setFooter('Error type: BUTTON');
+
+        channel.send(errorem).catch(() => channel.send("Jimmybot encountered a fatal error, for help, please join the support server, which you can get by pinging the bot.").catch(() => console.log("Jimmybot encountered a button error in a server!")))
+      }
+      else{
+        console.log("Jimmybot encountered a button error in a server!")
+      }
+    }
 
 
 
